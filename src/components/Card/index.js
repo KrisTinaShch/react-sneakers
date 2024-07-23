86c101640a204b608b521d867826bc0a97e4971f
Kris <kris.shcheglova@bk.ru>
2024-07-23 08:30:36 +0300

    feat: add loading cart

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,6 +1,7 @@
 import styles from './Card.module.scss'
 import React from 'react';
-function Card({ onFavorite, imageUrl, title, price, id, parentID, onPlus, favorite = false, added = false }) {
+import ContentLoader from "react-content-loader";
+function Card({ onFavorite, imageUrl, title, price, id, parentID, onPlus, favorite = false, added = false, isLoading = false }) {
 
   const [isAdded, setIsAdded] = React.useState(added);
   const [isFavorite, setIsFavorite] = React.useState(favorite);
@@ -17,21 +18,38 @@ function Card({ onFavorite, imageUrl, title, price, id, parentID, onPlus, favori
 
   return (
     <div className={styles.card}>
-      <div className={styles.favorite} onClick={onClickFavorite}>
-        <img src={isFavorite ? "/img/heart-liked.svg" : "/img/heart-unliked.svg"} alt="unliked"></img>
-      </div>
-      <img src={imageUrl} alt="sneaker-image" width={133} height={112}></img>
-      <h5>{title}</h5>
-      <div className="d-flex justify-between align-center">
-        <div className="d-flex flex-column">
-          <span>Цена: </span>
-          <b>{price} руб.</b>
-        </div>
-        <button className="button" onClick={onClickPlus}>
-          <img src={isAdded ? "./img/button-checked.svg" : "./img/button-unchecked.svg"} alt="" />
+      {isLoading ? <ContentLoader
+        speed={2}
+        width={400}
+        height={300}
+        viewBox="0 0 400 300"
+        backgroundColor="#f3f3f3"
+        foregroundColor="#ecebeb"
+      >
+        <rect x="0" y="0" rx="10" ry="10" width="150" height="150" />
+        <rect x="0" y="160" rx="5" ry="5" width="150" height="15" />
+        <rect x="0" y="182" rx="5" ry="5" width="100" height="15" />
+        <rect x="0" y="220" rx="5" ry="5" width="100" height="25" />
+        <rect x="117" y="220" rx="5" ry="5" width="30" height="25" />
+      </ContentLoader>
+      :<>
+          <div className={styles.favorite} onClick={onClickFavorite}>
+            <img src={isFavorite ? "/img/heart-liked.svg" : "/img/heart-unliked.svg"} alt="unliked"></img>
+          </div>
+          <img src={imageUrl} alt="sneaker-image" width={133} height={112}></img>
+          <h5>{title}</h5>
+          <div className="d-flex justify-between align-center">
+            <div className="d-flex flex-column">
+              <span>Цена: </span>
+              <b>{price} руб.</b>
+            </div>
+            <button className="button" onClick={onClickPlus}>
+              <img src={isAdded ? "./img/button-checked.svg" : "./img/button-unchecked.svg"} alt="" />
 
-        </button>
-      </div>
+            </button>
+          </div>
+      </>}
+    
     </div>
   )
 }
